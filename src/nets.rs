1792f50a36d0ba4c7d3f389356fc522823af4e5f
Unknown <mark.broadmeadow@qut.edu.au>
2022-07-23 13:05:57 +1000

    Added net state update logic

--- src/nets.rs
+++ src/nets.rs
@@ -2,14 +2,17 @@ use std::rc::Rc;
 
 pub enum PinState {
     Open,
+    WeakPullDown,
+    WeakPullUp,
+    UndefinedWeak,
     DriveL,
     DriveH,
     DriveAnalog(f32),
-    WeakPullDown,
-    WeakPullUp
+    UndefinedStrong
 }
 
 pub enum NetState {
+    Undefined,
     High,
     Low,
     Analog(f32)
@@ -18,4 +21,64 @@ pub enum NetState {
 pub struct Net {
     state: NetState,
     io: Vec<Rc<PinState>>
+}
+
+impl Net {
+    fn update(&mut self) {
+        let mut dps = PinState::Open;
+
+        for ps in &self.io {
+            match **ps {
+                PinState::Open => {},
+                PinState::WeakPullDown => {
+                    match dps {
+                        PinState::Open => dps = PinState::WeakPullDown,
+                        PinState::WeakPullUp => dps = PinState::UndefinedWeak,
+                        _ => {} // if driven, already pulled down, or undefined, state unchanged
+                    }
+                },
+                PinState::WeakPullUp => {
+                    match dps {
+                        PinState::Open => dps = PinState::WeakPullUp,
+                        PinState::WeakPullDown => dps = PinState::UndefinedWeak,
+                        _ => {} // if driven, already pulled up, or undefined, state unchanged
+                    }
+                },
+                PinState::UndefinedWeak => {
+                    match dps {
+                        PinState::DriveH => dps = PinState::DriveH,
+                        PinState::DriveL => dps = PinState::DriveL,
+                        PinState::DriveAnalog(v) => dps = PinState::DriveAnalog(v),
+                        _ => {} // unless driven, remains weakly undefined
+                    }
+                },
+                PinState::DriveL => {
+                    match dps {
+                        PinState::DriveH | PinState::DriveAnalog(_) => {dps = PinState::UndefinedStrong; break}, // The net cant get out of this state
+                        _ => dps = PinState::DriveL
+                    }
+                },
+                PinState::DriveH => {
+                    match dps {
+                        PinState::DriveL | PinState::DriveAnalog(_) => {dps = PinState::UndefinedStrong; break}, // The net cant get out of this state
+                        _ => dps = PinState::DriveH
+                    }
+                },
+                PinState::DriveAnalog(v) => {
+                    match dps {
+                        PinState::DriveL | PinState::DriveH | PinState::DriveAnalog(_)=> {dps = PinState::UndefinedStrong; break}, // The net cant get out of this state
+                        _ => dps = PinState::DriveAnalog(v)
+                    }
+                },
+                PinState::UndefinedStrong => break  // We shouldn't ever get here
+            }
+        }
+
+        match dps {
+            PinState::Open | PinState::UndefinedWeak |  PinState::UndefinedStrong => self.state = NetState::Undefined,
+            PinState::WeakPullDown | PinState::DriveL => self.state = NetState::Low,
+            PinState::WeakPullUp | PinState::DriveH => self.state = NetState::High,
+            PinState::DriveAnalog(v) => self.state = NetState::Analog(v),      
+        }
+    }
 }
